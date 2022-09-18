1b61edd74d2baf8bad878d40b9e6ea3823941e66
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-18 15:29:25 -0300

    chore: added declaration types to micro-framework

--- build/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/', (request, response) => {
-    return response.json({ message: true });
-});
-app.listen(3333, () => {
-    console.log('Server is running at http://127.0.0.1/3333');
-});
