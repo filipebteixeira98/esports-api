5e30619e4611209852068b16b9679fb6877e230c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-21 17:26:28 -0300

    feat: enabled query log on prisma & created route to get games

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,48 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: true });
+const prisma = new PrismaClient({
+  log: ['query'],
+});
+
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return response.json(games);
+});
+
+app.get('/ads', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  const gameId = request.params.id;
+
+  console.log(gameId);
+
+  return response.json([]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adId = request.params.id;
+
+  console.log(adId);
+
+  return response.json([]);
 });
 
 app.listen(3333, () => {
